b91183cf5c412943210065d641ed79114703163b
Liha <bravacoreana@gmail.com>
2021-06-03 00:58:33 +0200

    no playground

--- src/server.js
+++ src/server.js
@@ -9,8 +9,8 @@ const PORT = process.env.PORT;
 const apollo = new ApolloServer({
   typeDefs,
   resolvers,
-  playground: true,
-  introspection: true,
+  // playground: true,
+  // introspection: true,
   context: async ({ req }) => {
     return {
       loggedInUser: await getUser(req.headers.token),
