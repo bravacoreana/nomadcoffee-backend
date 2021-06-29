5bf302220d09a837d0940ada176238072a6886bd
Liha Kim <lihakim.kr@gmail.com>
2021-06-29 19:59:14 +0200

    [code] pagination

--- src/coffeeShop/seeCoffeeShops/seeCoffeeShops.resolvers.js
+++ src/coffeeShop/seeCoffeeShops/seeCoffeeShops.resolvers.js
@@ -1,16 +1,22 @@
 import client from "../../client";
+import { PER_PAGE } from "../../constants";
 
 export default {
   Query: {
     seeCoffeeShops: async (_, { page = 1 }) => {
       try {
-        return await client.coffeeShop.findMany({
-          take: 5,
-          skip: (page - 1) * 5,
+        const shops = await client.coffeeShop.findMany({
+          take: PER_PAGE,
+          skip: (page - 1) * PER_PAGE,
           orderBy: {
             updatedAt: "desc",
           },
         });
+        const shopsCount = await client.coffeeShop.count();
+        const lastPage = Math.ceil(shopsCount / PER_PAGE);
+
+        // return shops;
+        return { shops, shopsCount, lastPage };
       } catch (error) {
         console.log("[seeCoffeeShops]", error);
       }
--- src/coffeeShop/seeCoffeeShops/seeCoffeeShops.typeDefs.js
+++ src/coffeeShop/seeCoffeeShops/seeCoffeeShops.typeDefs.js
@@ -1,7 +1,13 @@
 import { gql } from "apollo-server-core";
 
 export default gql`
+  type seeCoffeeShopResult {
+    shops: [CoffeeShop]
+    lastPage: Int!
+    shopsCount: Int!
+  }
   type Query {
-    seeCoffeeShops(page: Int): [CoffeeShop]
+    # seeCoffeeShops(page: Int): [CoffeeShop]
+    seeCoffeeShops(page: Int): seeCoffeeShopResult!
   }
 `;
