274e458ebcc29aa14896b3a2cc748f7193f592d3
Liha Kim <lihakim.kr@gmail.com>
2021-07-01 00:39:31 +0200

    [add] page

--- src/users/searchUsers/searchUsers.resolvers.js
+++ src/users/searchUsers/searchUsers.resolvers.js
@@ -2,14 +2,13 @@ import client from "../../client";
 
 export default {
   Query: {
-    searchUsers: async (_, { keyword }) => {
+    searchUsers: async (_, { keyword, page = 1 }) =>
       await client.user.findMany({
         where: {
           username: {
             startsWith: keyword.toLowerCase(),
           },
         },
-      });
-    },
+      }),
   },
 };
--- src/users/searchUsers/searchUsers.typeDefs.js
+++ src/users/searchUsers/searchUsers.typeDefs.js
@@ -2,6 +2,6 @@ import { gql } from "apollo-server-core";
 
 export default gql`
   type Query {
-    searchUsers(keyword: String!): [User]
+    searchUsers(keyword: String!, page: Int): [User]
   }
 `;
